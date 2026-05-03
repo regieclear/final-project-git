8b1c72682e00405e566e0b2b62ce98f04c420698
Regie <regieclaro417@gmail.com>
2026-05-03 22:50:25 +0800

    Modifiying hero section

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("heroButton");
+
+  button.addEventListener("click", function () {
+    alert("This is your practice site — feel free to customize it!");
+  });
+});
